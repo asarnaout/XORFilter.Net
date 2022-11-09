168c48832b996a7b5394399869f230c7269be6b1
aarnaout <aarnaout@webstaurantstore.com>
2022-11-08 22:04:39 -0500

    Rewrite the unit test

--- XORFilter.Net/Hashing/Murmur32.cs
+++ XORFilter.Net/Hashing/Murmur32.cs
@@ -4,7 +4,7 @@ namespace XORFilter.Net.Hashing
 {
     public static class Murmur32
     {
-        private static readonly uint _seed = (uint)new Random().Next(int.MaxValue);
+        private static readonly uint _seed = (uint)new Random().Next(int.MaxValue); //TODO: Is this the best way to initialize the seed? Maybe pass the seed from the filter.
 
         public static uint Hash(byte[] input) => MurmurHash3.Hash32(input, _seed);
     }
